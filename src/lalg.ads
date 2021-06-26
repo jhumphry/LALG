3b6af98e87e57a862bc79e2bcfb57fcfc8d4bcc8
James Humphry <git@binecho.co.uk>
2021-06-26 21:54:10 +0100

    Remove unnecessary use type clause
    Signed-off-by: James Humphry <git@binecho.co.uk>

--- src/lalg.ads
+++ src/lalg.ads
@@ -159,7 +159,6 @@ package LALG is
 private
 
    package IntFort renames Interfaces.Fortran;
-   use type Interfaces.Fortran.Fortran_Integer;
 
    -- Fortran precision corresponding to the float type used
    type Precision_Specification is (Single, Double);
